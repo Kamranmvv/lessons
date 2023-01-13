d93b50d595ee059719e11c213425086f1974631b
Kamran Memmedov <azadov070134@gmail.com>
2023-01-13 22:13:36 +0400

    test

---
+++ 1ci ay dersde olanlar/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Sexsi melumatlar");
+            //Console.Write("Adinizi daxil edin: ");
+            //var ad = Console.ReadLine();
+            //Console.Write("Soyadinizi daxil edin: ");
+            //var soyad = Console.ReadLine();
+            //Console.Write("Yasinizi daxil edin: ");
+            //var yas = Console.ReadLine();
+            //Console.Write("Dogum yerinizi daxil edin: ");
+            //var dogumyeri = Console.ReadLine();
+            //Console.WriteLine("Ad: " + ad + "\n" + "Soyad: " + soyad + "\n" + "Yas: " + yas + "\n" + "DogumYeri: " + dogumyeri);
+            //Console.ReadLine();
+
+
+            //task2
+            //var red = "qirmizi";
+            //var car = "avtomobil";
+            //var expensive = "bahali";
+            //var phone = "telefon";
+            //Console.WriteLine(red + " " + car);
+            //Console.WriteLine(expensive + " " + phone);
+            //Console.ReadLine();
+
+            //task 2.2
+            //var red = "qirmizi";
+            //var car = "avtomobil";
+            //var expensive = "bahali";
+            //var phone = "telefon";
+            //var bosluq = " ";
+            //Console.Write(red);
+            //Console.Write(bosluq);
+            //Console.WriteLine(car);
+            //Console.Write(expensive);
+            //Console.Write(bosluq);
+            //Console.WriteLine(phone);
+            //Console.ReadLine();
+
+            //task2.3
+            //var red = "qirmizi ";
+            //var car = "avtomobil";
+            //var expensive = "bahali ";
+            //var phone = "telefon";
+            //Console.Write(red);
+            //Console.WriteLine(car);
+            //Console.Write(expensive);
+            //Console.WriteLine(phone);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
