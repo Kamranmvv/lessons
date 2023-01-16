f358316a72ae7a889f2b0572ae0bce635e93a61e
Kamran Memmedov <azadov070134@gmail.com>
2023-01-16 22:59:35 +0400

    LessonTask added

---
+++ January/JanuaryInLesson/16.01.2023/Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ January/JanuaryInLesson/16.01.2023/Task2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aze = "Azerbaycan";
+            string tur = "Turkiye";
+            string AzeTur = aze + " " + tur;
+            Console.WriteLine(AzeTur);
+            Console.ReadLine();
+        }
+    }
+}
