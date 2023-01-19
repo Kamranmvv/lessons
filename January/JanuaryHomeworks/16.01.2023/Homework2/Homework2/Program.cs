95012694ed44457bb0178a5532e3834bf9061d2e
Kamran Memmedov <azadov070134@gmail.com>
2023-01-20 00:03:23 +0400

    Homework added

---
+++ January/JanuaryHomeworks/16.01.2023/Homework2/Homework2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   Homework 2
+               
+                 int, bool, char, string və long tipində dəyişənlər yaradıb onlara dəyər verirsiniz.Sonra elə bir dəyişən yaratmalısınız ki, bütün bu 
+
+                 ona dəyər olaraq mənimsədə biləsiniz.*/
+
+
+            string nameSurname = "Kamran Mammadov ";
+            string text1 = " yaşında ";
+            string text2 = " gencdir? ";
+            string result = "Netice:";
+
+            long age = 21;
+            bool f = false;
+            char one = '1';
+            string longAge = age.ToString();
+            string boolFalse = f.ToString();
+            string charOne = one.ToString();
+            string x = nameSurname + longAge + text1 + charOne + text2 + result + boolFalse;
+            Console.Write(x);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
