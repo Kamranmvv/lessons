1110886bc9707a16a087de33ddcfa1d4a8342093
Kamran Memmedov <azadov070134@gmail.com>
2023-01-23 20:05:00 +0400

    homework edited

---
+++ January/JanuaryHomeworks/20.01.2023/Homework5/Homework5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int globalAge = 0;//Value type
+            string globalName = "";//Referance type
+           
+            {
+                int localAge = 21;
+                globalAge = localAge;
+                string localName = "Kamran";
+                globalName = localName;
+
+
+            }
+            {
+                int otherLocalAge;
+                otherLocalAge = globalAge;
+                string otherLocalName = globalName;
+                Console.WriteLine("Local Area: ");
+                Console.WriteLine(otherLocalName);
+                Console.WriteLine(otherLocalAge);
+                Console.ReadLine();
+
+            }
+            Console.WriteLine("Global Area: ");
+            Console.WriteLine(globalName);
+            Console.WriteLine(globalAge);
+            Console.ReadLine();
+
+        }
+    }
+}
