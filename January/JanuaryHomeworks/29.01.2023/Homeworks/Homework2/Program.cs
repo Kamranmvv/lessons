90f2a337ca10cf1806eaa80e071f2cfbad9c61b2
Kamran Memmedov <azadov070134@gmail.com>
2023-01-30 01:19:28 +0400

    homework completed

---
+++ January/JanuaryHomeworks/29.01.2023/Homeworks/Homework2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Homework 2:
+            Switch-case ilə sadə kalkulyator app'ı yazacaqsız. İlk öncə istifadəçiyə kiçik menyu çıxardın. Orda edə biləcəyi əməliyyatlar olacaq. Toplama, çıxma, vurma
+            və bölmə. İstifadəçi hansını seçsə həmin case'də ekrana nəticəni çıxardacaqsız. Nəticəni göstərəndən sonra aşağıda yenidən ana menyunu göstərəcəksiz. İstifadəçi
+            istədiyi qədər yəni proqramı bağlayana qədər bu döngü halında davam edəcək.
+            */
+
+            Console.WriteLine("-----------------------------The Best CalculatorApp-----------------------------");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            string calcValueStr = Console.ReadLine();
+            int calcValue = 0;
+            bool isNumber = int.TryParse(calcValueStr, out calcValue);
+            if (!isNumber)
+            {
+                Console.WriteLine("Include number!");
+            }
+            else if (calcValue < 0 || calcValue > 4)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+           
+             
+            {
+                switch (calcValue)
+                {
+                    case 1:
+                        Console.WriteLine(a + b);
+                        goto default;
+
+                    case 2:
+                        Console.WriteLine(a - b);
+                        goto default;
+
+                    case 3:
+                        Console.WriteLine(a * b);
+                        goto default;
+                    case 4:
+                        if (b != 0)
+                        {
+                            Console.WriteLine(a / b);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You can't divide by zero!");
+                        }
+                        goto default;
+
+
+                    default:
+                        Console.WriteLine("1. Addition");
+                        Console.WriteLine("2. Subtraction");
+                        Console.WriteLine("3. Multiplication");
+                        Console.WriteLine("4. Division");
+                        calcValueStr = Console.ReadLine();
+                        a = Convert.ToInt32(Console.ReadLine());
+                        b = Convert.ToInt32(Console.ReadLine());
+
+                        calcValue = 0;
+                        isNumber = int.TryParse(calcValueStr, out calcValue);
+                        if (!isNumber)
+                        {
+                            Console.WriteLine("Include number!");
+                        }
+                        else if (calcValue < 0 && calcValue > 4)
+                        {
+                            Console.WriteLine("Something went wrong!");
+                        }
+                        else
+                        {
+                            if (calcValue == 1)
+                                goto case 1;
+                            else if (calcValue == 2)
+                                goto case 2;
+                            else if (calcValue == 3)
+                                goto case 3;
+                            else if (calcValue == 4)
+                                goto case 4;
+                        }
+                        break;
+
+
+                }
+            }
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ January/JanuaryHomeworks/29.01.2023/Homeworks/Homeworks/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Homework 1:
+            Əvvəlcədən iki dəyişəndə username və password saxlayın. Bunlar heç bir zaman dəyişməyəcək şəkildə olsun. Yəni proqramda belə dəyişdirmək mümkün olmasın
+            Sonra istifadəçidən username və password'u daxil etməsini istəyin. Əgər istifadəçi doğru məlumatları yazsa ekrana "Sisteme daxil olundu." mesajı çıxsın.
+            Yox əgər səhv məlumat daxil etsə, yenidən username və password'u daxil etməsini istəyin. 3 dəfə səhv daxil etdikdən sonra "Hesab bloklandi" yazsın
+            */
+            Console.WriteLine("Login System");
+            const string username = "Kamran";
+            const string password = "Kamran2002";
+            Console.Write("Username daxil edin: ");
+            string usernameEntered = Console.ReadLine();
+            Console.Write("Password daxil edin: ");
+            string passwordEntered = Console.ReadLine();
+
+            if (username == usernameEntered && password == passwordEntered)
+            {
+                Console.WriteLine("Sisteme daxil olundu.");
+            }
+            else
+            {
+                Console.Write("Username daxil edin: ");
+                usernameEntered = Console.ReadLine();
+                Console.Write("Password daxil edin: ");
+                passwordEntered = Console.ReadLine();
+
+                if (username == usernameEntered && password == passwordEntered)
+                {
+                    Console.WriteLine("Sisteme daxil olundu.");
+                }
+                else
+                {
+                    Console.Write("Username daxil edin: ");
+                    usernameEntered = Console.ReadLine();
+                    Console.Write("Password daxil edin: ");
+                    passwordEntered = Console.ReadLine();
+                }
+                }
+                if (username == usernameEntered && password == passwordEntered)
+                {
+                    Console.WriteLine("Sisteme daxil olundu.");
+                }
+                else
+                {
+                Console.WriteLine("Hesabiniz blok olundu");
+                }
+
+            Console.ReadLine();
+
+            }
+        }
+    }
