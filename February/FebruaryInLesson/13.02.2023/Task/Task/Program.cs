d5a388a9d6cb820f01d9e9608aad03391adc43cc
Kamran Memmedov <azadov070134@gmail.com>
2023-02-21 11:38:06 +0400

    edit

---
+++ February/FebruaryInLesson/13.02.2023/Task/Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Orkhan" , "Farajov", 28 , "Undefined");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ February/FebruaryInLesson/13.02.2023/Task/Task/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public string FatherName;
+
+
+
+        public Student(string name ,string surname ,int age , string fatherName)
+        {
+            if(name == "Orkhan")
+            {
+                Console.WriteLine("Error");
+            }
+            else if (age>35)
+            {
+                Console.WriteLine("Telebenin yasi 34 den yuxari ola bilmez");
+            }
+            else
+            {
+                Name = name;
+                Surname = surname;
+                Age = age;
+                FatherName = fatherName;
+            }
+            
+        }
+
+    }
+}
