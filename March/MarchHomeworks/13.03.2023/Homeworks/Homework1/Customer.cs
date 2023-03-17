366ca0ea38c829e98e1529d4eb9f8a6775ee42a6
Kamran Memmedov <azadov070134@gmail.com>
2023-03-17 16:39:42 +0400

    Homework pushed

---
+++ March/MarchHomeworks/13.03.2023/Homeworks/Homework1/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+   public class Customer : Address
+    {
+        
+        
+        public override int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+       
+        public void ForEach (List<Customer> customers)
+        {
+            foreach (var item in customers)
+            {
+                Console.WriteLine("ID: {0} \r\nName: {1} \r\nSurname: {2} \r\nAge: {3} \r\nNo: {4} \r\nBuilding: {5} \r\nStreet: {6} \r\nCity: {7} \r\nCountry: {8} ",
+                    item.Id,
+                    item.Name,
+                    item.Surname,
+                    item.Age,
+                    item.No,
+                    item.Building,
+                    item.Street,
+                    item.City,
+                    item.Country);
+
+            }
+        }
+
+        
+    }
+}
--- March/MarchHomeworks/13.03.2023/Homeworks/Homework1/Program.cs
+++ March/MarchHomeworks/13.03.2023/Homeworks/Homework1/Program.cs
@@ -10,6 +10,39 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            
+            List<Customer> customers = new List<Customer>();
+
+            customers.Add(new Customer()
+            {
+                Id = 1,
+                Name = "Nihad",
+                Surname = "Adigozelov",
+                Age = 20,
+                No = 97,
+                Building = "13A",
+                Street = "Sulh",
+                City = "Sumqayit",
+                Country = "Azerbaycan"
+            });
+            customers.Add(new Customer()
+            {
+                Id = 2,
+                Name = "Kamran",
+                Surname = "Mammadov",
+                Age = 21,
+                No = 9,
+                Building = "68",
+                Street = "Zulfi Haciyev",
+                City = "Sumqayit",
+                Country = "Azerbaycan"
+});
+            Customer customer = new Customer();
+            customer.ForEach(customers);
+
+
+
+            Console.ReadLine();
         }
     }
 }
