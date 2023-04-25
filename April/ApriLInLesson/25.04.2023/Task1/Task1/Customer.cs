dd4ea94a9289ff40ba33955d2a1d5d29a1492f77
Kamran Mammadov <kamranmv1@gmail.com>
2023-04-25 23:31:53 +0400

    Lesson task2 added

---
+++ April/ApriLInLesson/25.04.2023/Task1/Task1/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+
+        public Customer(int id , string name , string surname , int age, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            IsActive = isActive;
+        }
+    }
+}
---
+++ April/ApriLInLesson/25.04.2023/Task1/Task1/DataSource.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class DataSource
+    {
+        List<Customer> Customers = new List<Customer>();
+
+        public DataSource ()
+        {
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                int id = i;
+                string name = FakeData.NameData.GetFirstName();
+                string surname = FakeData.NameData.GetSurname();
+                Random random = new Random();
+                int age = random.Next(0, 100);
+                bool isActive;
+                int x = random.Next(1, 3);
+                if (x==1)
+                {
+                    isActive = true;
+                }
+                else
+                {
+                    isActive = false;
+                }
+                Customer customer = new Customer(
+                    id,
+                    name,
+                    surname,
+                    age,
+                    isActive);
+                Customers.Add(customer);
+                
+            }
+        }
+        public List<Customer> TakePersonsForFirstLetter(char a)
+        {
+            return Customers.Where(m => m.Name.StartsWith(a.ToString())).ToList();
+        }
+        public List<Customer> IsActive(string x)
+        {
+            if (x=="1")
+            {
+                return Customers.Where(m => m.IsActive.ToString() == "true").ToList();
+            }
+            else
+            {
+               return Customers.Where(m => m.IsActive.ToString() == "false").ToList();
+            }
+            
+
+        }
+    }
+}
