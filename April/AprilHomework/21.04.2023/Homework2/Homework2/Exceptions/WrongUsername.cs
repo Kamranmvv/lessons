6a5427c1abf855660c3334e0a1911562826fde59
Kamran Mammadov <kamranmv1@gmail.com>
2023-04-25 20:27:58 +0400

    Homework add

---
+++ April/AprilHomework/21.04.2023/Homework2/Homework2/Exceptions/WrongUsername.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2.Exceptions
+{
+    public class WrongUsername : Exception
+    {
+        public WrongUsername()
+        {
+            Console.WriteLine("Username yanlışdır!");
+        }
+    }
+}
---
+++ April/AprilHomework/21.04.2023/Homework2/Homework2/Program.cs
@@ -0,0 +1,75 @@
+﻿using Homework2.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Homework 2:
+             Exception handling mövzusunu araşdırdıqdan sonra custom exceptionlar yazmağa çalışın. Hər hansısa bir 
+             kiçik app düşünüb ona uyğun custom exception'lar yaza bilərsiniz.*/
+            
+            Console.WriteLine("Register Time");
+            Console.WriteLine("Yeni Username daxil edin: ");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Yeni Şifrenizi daxil edin: ");
+            string newSurname = Console.ReadLine();
+
+            Console.Clear();
+            Begin:
+            try
+            {
+                
+                Console.WriteLine("Login Time");
+                Console.WriteLine("Usernamenizi daxil edin: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Şifrenizi daxil edin: ");
+                string surname = Console.ReadLine();
+
+                if (name != newName)
+                {
+
+                    
+                    throw new WrongUsername();
+                    
+                }
+
+                if (surname != newSurname)
+                {
+                    
+                    throw new WrongPassword();
+                }
+            }
+            catch (WrongUsername ex)
+            {
+
+                // Console.WriteLine(ex.Message);
+                goto Begin;
+
+            }
+            catch (WrongPassword ex)
+            {
+                // Console.WriteLine(ex.Message);
+                goto Begin;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bilinməyən xəta!");
+                goto Begin;
+
+
+            }
+            Console.Clear();
+            Console.WriteLine("Giriş olundu!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
