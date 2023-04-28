0d61ace6e7b9583a8e424b7d759ca16669c9e6f7
Kamran Mammadov <kamranmv1@gmail.com>
2023-04-28 16:58:52 +0400

    Homework5 added

---
+++ April/AprilHomework/25.04.2023/Homework5/Homework5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 1000; i++)
+            {
+                Person person = new Person();
+                person.Name = FakeData.NameData.GetFirstName();
+                person.Surname = FakeData.NameData.GetSurname();
+
+                people.Add(person);
+            }
+            people.ForEach(m => m.Fullname = $"{m.Name} {m.Surname}");
+        }
+    }
+}
