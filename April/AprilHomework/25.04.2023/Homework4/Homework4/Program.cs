490101be8d51ecd055942f1139c242c027d255c7
Kamran Mammadov <kamranmv1@gmail.com>
2023-04-28 10:09:42 +0400

    Homework4 added

---
+++ April/AprilHomework/25.04.2023/Homework4/Homework4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                string name = FakeData.NameData.GetFirstName();
+                names.Add(name);
+            }
+            names.RemoveAll(m => m.Length >= 5);
+        }
+    }
+}
