c4ba0af0bc3da1c09306318575612d8211c3b5d3
Kamran Mammadov <kamranmv1@gmail.com>
2023-04-28 10:02:46 +0400

    Homework 1 added

---
+++ April/AprilHomework/25.04.2023/Homework1/Homework1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAnyMethod myDelegate = Add;
+            myDelegate();
+            myDelegate = Edit;
+            myDelegate();
+            myDelegate = Delete;
+            myDelegate();
+        }
+        delegate void RunAnyMethod();
+        public static void Add()
+        {
+            Console.WriteLine("Add method");
+        }
+
+        public static void Edit()
+        {
+            Console.WriteLine("Edit method");
+        }
+
+        public static void Delete()
+        {
+            Console.WriteLine("Delete method");
+        }
+
+    }
+}
