fadffe8dd8d909037b4fca22017cd07211a49e40
Kamran Mammadov <kamranmv1@gmail.com>
2023-05-26 20:33:29 +0400

    EduSys created

---
+++ May/24.05.2023/EduSys/EduSys.Core/Models/ProductFeature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSys.Core.Models
+{
+    public class ProductFeature : BaseEntity
+    {
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
